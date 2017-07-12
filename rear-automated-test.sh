d20d5383ed26b3a8e365881e84a2ca521467b43a
Gratien D'haese <gratien.dhaese@gmail.com>
2017-07-12 15:53:00 +0200

    boot_server difference between Linux-Darwin - issue #28

--- rear-automated-test.sh
+++ rear-automated-test.sh
@@ -439,7 +439,12 @@ ISO)
 ####
    #echo "$(red WARNING: Sorry 'not' yet completely tested by $PRGNAME)"
    case $VAGRANT_DEFAULT_PROVIDER in
-       virtualbox) boot_server="10.0.2.2"
+       virtualbox) 
+                   case $(uname -s) in
+                     Linux)  boot_server="10.0.2.2" ;;
+                     Darwin) boot_server="192.168.33.1" ;;
+                   esac
+
                    pxe_tftpboot_path=$( define_pxe_tftpboot_path )
                    [[ ! -d "$pxe_tftpboot_path" ]] && mkdir -p -m 755 "$pxe_tftpboot_path"
                    [[ ! -d "$pxe_tftpboot_path/pxelinux.cfg" ]] && mkdir -p -m 755 "$pxe_tftpboot_path/pxelinux.cfg"
