3a2b9a5464ee2c0eeca343cc93512cecc81c5391
Gratien D'haese <gratien.dhaese@gmail.com>
2018-02-21 10:21:36 +0100

    iputils integrity check #49

---
+++ inspec/compliance-checks/controls/rpm.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+# copyright: 2018, IT3 Consultants, Gratien D'haese
+# license: All rights reserved
+
+title 'RPM integrity checks'
+
+control 'iputils integrity' do
+  title 'RPM integrity test on iputils package'
+  desc "The ping executable requires cap_net_raw privilege. With rpm -V we can check the integrity of the package. ReaR recover sometimes do not restore the capabilities."
+
+  describe package('iputils') do
+    it { should be_installed }
+  end
+
+  describe command("rpm -V iputils") do
+    its(:stdout) { should eq '' }
+  end
+end
