f27be89b1ffb0bec732ac766c005251ef9aa8765
root <root@okido.lan>
2019-04-30 11:51:50 +0200

    add for inspec redhat os.family and disbale selinux on ubuntu

--- inspec/compliance-checks/controls/rpm.rb
+++ inspec/compliance-checks/controls/rpm.rb
@@ -8,11 +8,14 @@ control 'iputils integrity' do
   title 'RPM integrity test on iputils package'
   desc "The ping executable requires cap_net_raw privilege. With rpm -V we can check the integrity of the package. ReaR recover sometimes do not restore the capabilities."
 
-  describe package('iputils') do
-    it { should be_installed }
+  if os.redhat?
+    describe package('iputils') do
+      it { should be_installed }
+    end
+
+    describe command("rpm -V iputils") do
+      its(:stdout) { should eq '' }
+    end
   end
 
-  describe command("rpm -V iputils") do
-    its(:stdout) { should eq '' }
-  end
 end
